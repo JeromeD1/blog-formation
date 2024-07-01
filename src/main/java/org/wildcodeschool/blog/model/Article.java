cb23684bce4c9ab66bfb1b1d54cce0a8f24f16e6
Jerome Desouches <jerome.desouches@gmail.com>
2024-07-01 11:48:59 +0200

    spring 02

---
+++ src/main/java/org/wildcodeschool/blog/model/Article.java
@@ -0,0 +1,37 @@
+package org.wildcodeschool.blog.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void create(){
+        final LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void update(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
+
