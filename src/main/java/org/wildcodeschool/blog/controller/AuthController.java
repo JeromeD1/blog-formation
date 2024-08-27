d6993f069a627604fd188fa7c92630b581fa4367
Jerome Desouches <jerome.desouches@gmail.com>
2024-08-27 06:58:22 +0200

    spring 013

--- src/main/java/org/wildcodeschool/blog/controller/AuthController.java
+++ src/main/java/org/wildcodeschool/blog/controller/AuthController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wildcodeschool.blog.model.DTO.UserLoginDTO;
 import org.wildcodeschool.blog.model.DTO.UserRegistrationDTO;
 import org.wildcodeschool.blog.model.entity.User;
+import org.wildcodeschool.blog.security.AuthenticationService;
 import org.wildcodeschool.blog.service.UserService;
 
 import java.util.Set;
@@ -17,9 +19,11 @@ import java.util.Set;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationService authenticationService) {
         this.userService = userService;
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("/register")
@@ -30,5 +34,14 @@ public class AuthController {
                 Set.of("ROLE_USER") // Par défaut, chaque utilisateur aura le rôle "USER"
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate(@RequestBody UserLoginDTO userLoginDTO) {
+        String token = authenticationService.authenticate(
+                userLoginDTO.getEmail(),
+                userLoginDTO.getPassword()
+        );
+        return ResponseEntity.ok(token);
     }
 }
---
+++ src/main/java/org/wildcodeschool/blog/security/JwtService.java
@@ -0,0 +1,69 @@
+package org.wildcodeschool.blog.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    public String generateToken(UserDetails userDetails) {
+//        System.out.println("userDetails: " + userDetails.getUsername());
+//        System.out.println("authorities: " + userDetails.getAuthorities());
+//        System.out.println("secretKey: " + secretKey);
+//        System.out.println("jwtExpiration: " + jwtExpiration);
+//        String jwt = Jwts.builder()
+//                .setSubject(userDetails.getUsername())
+//                .claim("roles", userDetails.getAuthorities())
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+//                .signWith(SignatureAlgorithm.HS256, secretKey)
+//                .compact();
+//
+//        System.out.println("jwt" + jwt);
+//        return jwt;
+        try {
+            return Jwts.builder()
+                    .setSubject(userDetails.getUsername())
+                    .claim("roles", userDetails.getAuthorities())
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                    .signWith(SignatureAlgorithm.HS256, secretKey.getBytes())
+                    .compact();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erreur lors de la génération du token JWT", e);
+        }
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+//            Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes()).build().parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(secretKey.getBytes()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
